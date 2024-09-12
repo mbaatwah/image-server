f0b8ac3509c561a642ce8464e1c246b11663c543
Mohammed Baatwah <mohammed.baatwah@gmail.com>
2024-09-12 12:22:08 +0000

    Add image model and testing tools

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,3 @@
 class Image < ApplicationRecord
+    validates :url, presence: true
 end
--- db/migrate/20240829210040_create_images.rb
+++ db/migrate/20240829210040_create_images.rb
@@ -1,6 +1,8 @@
 class CreateImages < ActiveRecord::Migration[7.2]
   def change
-    create_table :images do |t|
+    create_table :images, id: false do |t|
+      t.string :id, primary_key: true, null: false, default: :uuid
+      t.string :url
       t.timestamps
     end
   end
